172e658d8aced86556a3766e63bdecc8e0093a07
Sander van Harmelen <svanharmelen@schubergphilis.com>
2014-03-28 17:58:34 +0100

    Initial commit...

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+sbp_treesizefree CHANGELOG
+==========================
+
+0.1.0
+-----
+- Sander van Harmelen / Ane van Straten - Initial release of treesizefree
---
+++ attributes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: sbp_treesizefree
+# Attribute:: default
+#
+# Copyright 2013, Schuberg Philis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['treesizefree']['url']          = 'http://www.jam-software.de/treesize_free/TreeSizeFree.zip'
+default['treesizefree']['install_dir']  = 'C:\Program Files\TreeSizeFree'
