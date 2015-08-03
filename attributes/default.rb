2298aa62427683e3aad693fdbe3a49d9e9f2ff5d
sezenger <sebastian.zengerle@t-systems.com>
2015-08-03 11:26:09 +0200

    added switch for shortcut

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,3 +20,4 @@
 default['treesizefree']['url']          = 'http://www.jam-software.de/treesize_free/TreeSizeFree.zip'
 default['treesizefree']['checksum']     = '662e00ef4f86d7c2a84dd1067ba7a8c5251176a14b533d4a4d8f746c88765cbc'
 default['treesizefree']['install_dir']  = 'C:\Program Files\TreeSizeFree'
+default['treesizefree']['shortcuts']  	= 'false'
