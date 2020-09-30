2fa9da7a23c5417dead1b796ea08dffd2916444a
Eric Kuiper <ekuiper@schubergphilis.com>
2020-09-30 11:38:38 +0200

    Update to chef 15

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
 name             'sbp_treesizefree'
-maintainer       'Ane van Straten'
-maintainer_email 'avanstraten@schubergphilis.com'
-license          'Apache 2.0'
+maintainer       'Eric Kuiper'
+maintainer_email 'ekuiper@schubergphilis.com'
+license          'Apache-2.0'
 description      'Installs/Configures TreeSizeFree'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '1.0.0'
 
 supports         'windows'
 
