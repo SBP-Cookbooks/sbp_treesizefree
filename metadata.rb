41ed3c1d77ca3c14d01fbb83bcff50ffe685876b
Eric Kuiper <ekuiper@schubergphilis.com>
2020-09-30 11:42:06 +0200

    add chef version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'ekuiper@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs/Configures TreeSizeFree'
 version          '1.0.0'
+chef_version     '>= 14.0'
 
 supports         'windows'
 
